234627b57840b5b6bb7c8dcabdb6d3d8f6c4684b
Ami jadav <ami.jadav@xillentech.com>
2025-04-21 17:20:51 +0530

    Work in progress
    - Implemented the Controller and Observer classes to handle logical keyboard keys from A to Z.
    
    - Added a stream to dynamically update the UI based on key press events.

--- lib/design_patterns/observer_pattern/observer_example.dart
+++ lib/design_patterns/observer_pattern/observer_example.dart
@@ -1,5 +1,89 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+typedef KeyboardEvent<T> = ({LogicalKeyboardKey key, T value});
+
+final instance = KeyboardController();
+
+class KeyboardController {
+  late final StreamSubscription subscription;
+
+  // Map to track which keys are currently pressed
+  final Map<LogicalKeyboardKey, Color?> keyStates = {
+    for (int count = 0; count < 26; count++)
+      LogicalKeyboardKey(LogicalKeyboardKey.keyA.keyId + count): null
+  };
+
+  KeyboardController() {
+    subscription = Observer.subscribeToKeyEvents(setKeyState);
+  }
+
+  void _onNewColorEvent(LogicalKeyboardKey key, bool isDown, Color? color) {
+    Observer.addNewColorEvent((key: key, value: isDown ? color : null));
+  }
+
+  // Call this on key press/release
+  void setKeyState(KeyEvent event) {
+    final key = event.logicalKey;
+    final isDown = event is KeyDownEvent;
+    if (keyStates.containsKey(key)) {
+      if (key == LogicalKeyboardKey.keyA) {
+        _onNewColorEvent(key, isDown, Colors.greenAccent);
+        return;
+      }
+      if (key == LogicalKeyboardKey.keyS) {
+        _onNewColorEvent(key, isDown, Colors.blue);
+        return;
+      }
+      if (key == LogicalKeyboardKey.keyD) {
+        _onNewColorEvent(key, isDown, Colors.amber);
+        return;
+      }
+      if (key == LogicalKeyboardKey.keyF) {
+        _onNewColorEvent(key, isDown, Colors.brown);
+        return;
+      }
+      if (key == LogicalKeyboardKey.keyU) {
+        _onNewColorEvent(key, isDown, Colors.deepOrange);
+        return;
+      }
+      if (key == LogicalKeyboardKey.keyT) {
+        _onNewColorEvent(key, isDown, Colors.pink);
+        return;
+      }
+      if (key == LogicalKeyboardKey.keyN) {
+        _onNewColorEvent(key, isDown, Colors.blueGrey);
+        return;
+      }
+    }
+  }
+}
+
+class Observer {
+  static final keyStream = StreamController<KeyEvent>.broadcast();
+
+  static StreamSubscription subscribeToKeyEvents(Function(KeyEvent) listener) {
+    return keyStream.stream.listen(listener);
+  }
+
+  static StreamSubscription subscribeToColorEvents(
+      Function(KeyboardEvent<Color?>) listener) {
+    return colorStream.stream.listen(listener);
+  }
+
+  static void addNewKeyEvent(KeyEvent event) {
+    keyStream.add(event);
+  }
+
+  static final colorStream =
+      StreamController<KeyboardEvent<Color?>>.broadcast();
+
+  static void addNewColorEvent(KeyboardEvent<Color?> event) {
+    colorStream.add(event);
+  }
+}
 
 class ObserverScreenExample extends StatefulWidget {
   const ObserverScreenExample({super.key});
@@ -9,29 +93,8 @@ class ObserverScreenExample extends StatefulWidget {
 }
 
 class _ObserverScreenExampleState extends State<ObserverScreenExample> {
-  // Map to track which keys are currently pressed
-  final Map<LogicalKeyboardKey, bool> _keyStates = {
-    LogicalKeyboardKey.keyA: false,
-    LogicalKeyboardKey.keyS: false,
-    LogicalKeyboardKey.keyD: false,
-    LogicalKeyboardKey.keyF: false,
-  };
-
-  // Call this on key press/release
-  void setKeyState(LogicalKeyboardKey key, bool isDown) {
-    if (_keyStates.containsKey(key)) {
-      setState(() {
-        _keyStates[key] = isDown;
-      });
-    }
-  }
-
   // Flutter handles raw key events here
-  void _onNewKeyEvent(KeyEvent event) {
-    final key = event.logicalKey;
-    final isDown = event is KeyDownEvent;
-    setKeyState(key, isDown);
-  }
+  void _onNewKeyEvent(KeyEvent event) => Observer.addNewKeyEvent(event);
 
   @override
   Widget build(BuildContext context) {
@@ -42,29 +105,34 @@ class _ObserverScreenExampleState extends State<ObserverScreenExample> {
         autofocus: true,
         onKeyEvent: _onNewKeyEvent,
         child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: _keyStates.entries.map((entry) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: AnimatedContainer(
-                  duration: const Duration(milliseconds: 100),
-                  width: 60,
-                  height: 60,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: entry.value ? Colors.greenAccent : Colors.grey[300],
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: Text(
-                    entry.key.keyLabel.toUpperCase(),
-                    style: const TextStyle(
-                        fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              );
-            }).toList(),
+          child: Wrap(
+            children: List.generate(27, (count) {
+              LogicalKeyboardKey currentKey =
+                  LogicalKeyboardKey(LogicalKeyboardKey.keyA.keyId + count);
+              return StreamBuilder<KeyboardEvent<Color?>>(
+                  stream: Observer.colorStream.stream,
+                  builder: (context, snapshot) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 100),
+                        width: 60,
+                        height: 60,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: snapshot.data?.value ?? Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(color: Colors.black),
+                        ),
+                        child: Text(
+                          currentKey.keyLabel.toUpperCase(),
+                          style: const TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    );
+                  });
+            }),
           ),
         ),
       ),
